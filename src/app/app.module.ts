917ef4c09559eef0a10979d420f096db3f1c1172
Pubudu Ranasinghe <pubudutr@gmail.com>
2018-05-06 11:56:10 +0530

    create dynamic container

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CompHostDirective } from './directives/comp-host.directive';
+import { DynamicContainerComponent } from './dynamic-container/dynamic-container.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CompHostDirective
+    CompHostDirective,
+    DynamicContainerComponent
   ],
   imports: [
     BrowserModule
